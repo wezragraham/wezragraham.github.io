78c1fa22f5826fd3fec6e05ad807d8f99a9335d9
wezragraham <wezragraham@gmail.com>
2023-05-07 17:46:49 -0600

    hmmm

--- webgl-demo.js
+++ webgl-demo.js
@@ -23,7 +23,7 @@ function main() {
     }
 
     // Set clear color to red, fully opaque
-    gl.clearColor(01.0, 0.0, 0.0, 1.0);
+    gl.clearColor(1.0, 0.0, 0.0, 1.0);
     // Clear the color buffer with specified clear color
     gl.clear(gl.COLOR_BUFFER_BIT);
 
