1084f7a9713f0d3226846f787441f3359e11d81f
wezragraham <wezragraham@gmail.com>
2023-05-07 17:52:54 -0600

    ???

--- webgl-demo.js
+++ webgl-demo.js
@@ -23,12 +23,11 @@ function main() {
     }
 
     // Set clear color to red, fully opaque
-    gl.clearColor(1.0, 0.0, 0.0, 1.0);
+    gl.clearColor(1.0, 0.0, 0.0, 1.0));
     // Clear the color buffer with specified clear color
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     // Vertex shader program
-
     const vsSource = `
     attribute vec4 aVertexPosition;
     attribute vec4 aVertexColor;
